4c8c3842e67e8404372b83a4b7b2f674dfeb8206
yogesh7122002 <126946932+yogesh7122002@users.noreply.github.com>
2024-02-27 14:40:24 +0000

    ok

--- youtube_manager.py
+++ youtube_manager.py
@@ -1,7 +1,7 @@
 # import pymongo as pm
 from pymongo import MongoClient
 from bson import ObjectId
-client  = MongoClient("mongodb+srv://heyboy:heyboy@youtube.jdd8wbf.mongodb.net/")
+client  = MongoClient("mongodb+srv://youtube:youtube@youtube.jdd8wbf.mongodb.net/")
 # I've deleted This user 
 # contact deshmaneyogesh2002@gmail.com
 # update The role of This User form reader to can able to write
